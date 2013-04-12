ae9f738a14f194b3972805751d3f2b7fa583a5c1
PXTO <PXTO@PXTO-VAIO>
2013-04-12 10:10:19 -0500

    PxtoG

--- demo/src/demo/Demo.java
+++ demo/src/demo/Demo.java
@@ -14,7 +14,8 @@ public class Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Demo");
+        System.out.println("PXTOG");
+        System.out.println("sdsd");
     }
 }
